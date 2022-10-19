15eaab086b2933326c799c162242dcb3d453f018
Radek Cihlář <radekcihlar@radimsu.eu>
2022-10-19 12:24:51 +0200

    experimental needs Array print

--- src/GuessinTools.java
+++ src/GuessinTools.java
@@ -1,42 +1,28 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 
 public class GuessinTools {
+    private static int counter = 0;
+    private int fieldSize;
+    private ArrayList<Integer> listOfOptions = new ArrayList<>();
+    private int[][] showUser;
+    private int[][] guessingField;
 
-    public GuessinTools() {
-    }
-    public static int counter = 0;
-    public int userWants;
-    private ArrayList<Character> listOfOptions = new ArrayList<>();
-    private char[][] showUser =  {{'#', '#', '#', '#'}, {'#', '#', '#', '#'}, {'#', '#', '#', '#'}, {'#', '#', '#', '#'}};
-    private char[][] guessingField = new char[4][4];
-
-    // Generuje hrací pole, které neukazujeme hráči
-    public void generateHiddenField(){
-        for (int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                int random = (int) Math.floor(Math.random() * listOfOptions.size());
-                guessingField[i][j] = listOfOptions.get(random);
-                listOfOptions.remove(random);
-            }
-        }
-    }
 
     // Ukazuje herní pole, které je skryté a hráč vidí
     public void showPlayingField(){
-    for (int i = 0; i < 4; i++){
-        for(int j = 0; j < 4; j++){
-            System.out.print(showUser[i][j] + " ");
-        }
-        System.out.println();
+        for (int i = 0; i < fieldSize; i++){
+            for(int j = 0; j < fieldSize; j++){
+                System.out.print(showUser[i][j] + " ");
+            }
+            System.out.println();
         }
     }
     // Ukazuje skryté herní pole, které hráč ne
     public void showHiddenField(){
-        for (int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
+        for (int i = 0; i < fieldSize; i++){
+            for(int j = 0; j < fieldSize; j++){
                 System.out.print(guessingField[i][j] + " ");
             }
             System.out.println();
@@ -46,38 +32,38 @@ public class GuessinTools {
 
     public void guess(int pos1, int pos2, int pos3, int pos4) throws InterruptedException {
 
-    if((pos1 < 4) && (pos2 < 4) && (pos3 < 4) && (pos4 < 4)) {
-        if ((pos1 == pos3 && pos2 == pos4)) {
-            System.out.println("Enter different card coordinates!");
-            System.out.println();
+        if((pos1 < fieldSize) && (pos2 < fieldSize) && (pos3 < fieldSize) && (pos4 < fieldSize)) {
+            if ((pos1 == pos3 && pos2 == pos4)) {
+                System.out.println("Enter different card coordinates!");
+                System.out.println();
 
-            showPlayingField();
-        } else if (guessingField[pos1][pos2] == guessingField[pos3][pos4]) {
-            showUser[pos1][pos2] = guessingField[pos1][pos2];
-            showUser[pos3][pos4] = guessingField[pos3][pos4];
+                showPlayingField();
+            } else if (guessingField[pos1][pos2] == guessingField[pos3][pos4]) {
+                showUser[pos1][pos2] = guessingField[pos1][pos2];
+                showUser[pos3][pos4] = guessingField[pos3][pos4];
 
-            System.out.println("Correct!");
-            counter++;
+                System.out.println("Correct!");
+                counter++;
 
-            showPlayingField();
+                showPlayingField();
+            } else {
+                System.out.println("Inccorect!");
+                System.out.println();
+                showUser[pos1][pos2] = guessingField[pos1][pos2];
+                showUser[pos3][pos4] = guessingField[pos3][pos4];
+                showPlayingField();
+                TimeUnit.SECONDS.sleep(5);
+                System.out.println();
+                showUser[pos1][pos2] = '#';
+                showUser[pos3][pos4] = '#';
+                showPlayingField();
+            }
         } else {
-            System.out.println("Inccorect!");
-            System.out.println();
-            showUser[pos1][pos2] = guessingField[pos1][pos2];
-            showUser[pos3][pos4] = guessingField[pos3][pos4];
-            showPlayingField();
-            TimeUnit.SECONDS.sleep(5);
-            System.out.println();
-            showUser[pos1][pos2] = '#';
-            showUser[pos3][pos4] = '#';
-            showPlayingField();
+            System.out.println("Enter valid numbers!");
         }
-    } else {
-        System.out.println("Enter valid numbers!");
-    }
     }
     public boolean alreadyWon(){
-        return counter == 7;
+        return counter == (fieldSize*2) - 1;
     }
 
     public static int getCounter() {
@@ -87,14 +73,56 @@ public class GuessinTools {
     public static void setCounter(int counter) {
         GuessinTools.counter = counter;
     }
-    public void generateListOfOptions(int userWants){
+    // Generate list of possible combinations!
+    public void generateListOfOptions(){
         for (int j = 0; j < 2; j++){
-            for (int i = 1; i < (userWants*userWants) / 2; i++){
-                listOfOptions.add('i');
+            for (int i = 1; i <= (fieldSize*fieldSize) / 2; i++){
+                listOfOptions.add(i);
             }
         }
 
     }
+    //Generate field that we show!
+    public void generateShowUser(){
+        char[][] showUser = new char[fieldSize][fieldSize];
+        for (int i = 0; i < fieldSize; i++){
+            for(int j = 0; j < fieldSize; j++){
+                showUser[i][j] = '#';
+            }
+        }
+    }
+    // Generate playing field
+    public void generateHiddenField(){
+        int[][] guessingField = new int[fieldSize][fieldSize];
+        for (int i = 0; i < fieldSize; i++){
+            for(int j = 0; j < fieldSize; j++){
+                int random = (int) Math.floor(Math.random() * listOfOptions.size());
+                guessingField[i][j] = listOfOptions.get(random);
+                listOfOptions.remove(random);
+            }
+        }
+    }
+    // Generate game
+    public void generateGame(){
+        generateListOfOptions();
+        generateShowUser();
+        generateHiddenField();
+    }
 
+    public int getFieldSize() {
+        return fieldSize;
+    }
+
+    public void setFieldSize(int fieldSize) {
+        this.fieldSize = fieldSize;
+    }
+
+    public int[][] getShowUser() {
+        return showUser;
+    }
+
+    public int[][] getGuessingField() {
+        return guessingField;
+    }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,75 @@ public class Main {
         int pos1, pos2, pos3, pos4;
         boolean flag = true;
         boolean playAgain = true;
+        int fieldSize;
 
     while(playAgain){
+
+        do {
+            System.out.print("What size of field would you like? (2,4,6,8,10): ");
+            fieldSize = scanner.nextInt();
+            scanner.nextLine();
+            play.setFieldSize(fieldSize);
+            if (fieldSize % 2 != 0 && fieldSize <= 10 && fieldSize > 1){
+                System.out.println("Please provide different number");
+            }
+        }while(fieldSize % 2 != 0);
+            play.generateGame();
         while(!play.alreadyWon()){
             //Show board to user only the first time
             if(flag){
-                play.showPlayingField();
+               play.showPlayingField();
                 flag = false;
             }
             //get user input
-            System.out.print("Enter coordinates od card A (1-4), (1-4): ");
-            pos1 = scanner.nextInt() - 1;
-            scanner.nextLine();
-            pos2 = scanner.nextInt() - 1;
-            scanner.nextLine();
-            System.out.print("Enter coordinates od card B (1-4), (1-4): ");
-            pos3 = scanner.nextInt() - 1;
-            scanner.nextLine();
-            pos4 = scanner.nextInt() - 1;
-            scanner.nextLine();
+            System.out.println("Enter positive number coordinates of card A (1-" + fieldSize + "), (1-" + fieldSize + "): ");
+
+            do {
+                System.out.print("Please enter a Row: ");
+                while (!scanner.hasNextInt()) {
+                    System.out.println("That's not a number!");
+                    System.out.println();
+                    System.out.print("Enter row again: ");
+                    scanner.next(); // this is important!
+                }
+                pos1 = scanner.nextInt() - 1;
+            } while (pos1 < -1);
+
+            do {
+                System.out.print("Please enter a column: ");
+                while (!scanner.hasNextInt()) {
+                    System.out.println("That's not a number!");
+                    System.out.println();
+                    System.out.print("Enter column again: ");
+                    scanner.next(); // this is important!
+                }
+                pos2 = scanner.nextInt() - 1;
+            } while (pos2 < -1);
+
+            System.out.println("Enter positive number coordinates of card B (1-" + fieldSize + "), (1-" + fieldSize + "): ");
+
+            do {
+                System.out.print("Please enter a Row: ");
+                while (!scanner.hasNextInt()) {
+                    System.out.println("That's not a number!");
+                    System.out.println();
+                    System.out.print("Enter row again: ");
+                    scanner.nextLine(); // this is important!
+                }
+                pos3 = scanner.nextInt() - 1;
+            } while (pos3 < -1);
+
+            do {
+                System.out.print("Please enter a column: ");
+                while (!scanner.hasNextInt()) {
+                    System.out.println("That's not a number!");
+                    System.out.println();
+                    System.out.print("Enter column again: ");
+                    scanner.nextLine(); // this is important!
+                }
+                pos4 = scanner.nextInt() - 1;
+            } while (pos4 < -1);
+
             play.guess(pos1, pos2, pos3, pos4);
         }
         //If user wins show whole field and ask for playing Again.
